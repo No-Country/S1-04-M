70aa5015536dd980adf352da1dd1dd02746c9fab
Erick-Rangel <rebel863@hotmail.com>
2022-03-31 22:27:21 -0600

    cambios en historyTransactions

--- frontend/src/components/HistoryTransactions.jsx
+++ frontend/src/components/HistoryTransactions.jsx
@@ -1,9 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-<<<<<<< HEAD
-=======
-/* import { Transactions } from './Transactions' */
->>>>>>> 611d5f2f875df9242f68849627f8369353f25f65
 
 export const HistoryTransactions = () => {
   return (
