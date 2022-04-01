75d1b858b7b66a8ed2c9b2179c1e20296de13bfd
Nahuel D. Bogado <ndevbogado@gmail.com>
2022-04-01 03:05:57 -0300

    sign in implementation

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -1,45 +1,59 @@
-const {Router} = require ('express');
-const router = Router();
-const { getUsers,  deleteUser, getUser, createUser, updateUser, getUserByUsername ,login} = require('../controllers/users.controller');
-const auth = require ('../middlewares/auth')
+const express = require('express');
+const users = require('../models/users');
+const router = express.Router();
+const User = require('../models/users');
+const passport = require('passport');
 
-const passport = require('passport')
-const jwt = require('jsonwebtoken')
+router.get('/users/signin', (req,res)=>{
+    res.render('users/signin');
+});
 
+router.post('/users/signin', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/signin'
+    // ,
+    // failureFlash: true
+}));
+router.get('/users/signup', (req,res)=>{
+    res.render('users/signup');
+});
 
-router.route ('/')
-//get ((req, res) => res.send ('Usuarios') )
+router.post('/users/signup', async (req,res)=>{
+    const {name, email, password, confirm_password} = req.body; // DATA REQUERIDA DEL FORMULARIO DE REGISTRO
+    const errors = [];
+    const PASSWORD_LENGTH = 4;
+    // const fields = [name, email, password, confirm_password];
 
+    // const fields.filter(field => field.length <= 0);
 
-.get ( passport.authenticate('jwt', { session: false }), getUsers)
-.post (passport.authenticate('jwt', { session: false }), createUser )
+    if(password !== confirm_password) {
+        errors.push({text: 'Password do not match'});
+    }
+    if(password.length < PASSWORD_LENGTH) {
+        errors.push({text: "Password should be, at least, 4 characters long."})
+    }
+    if(errors.length > 0 ) {
+        res.render('users/signup', {errors, name, email, password, confirm_password}); // DATA ENVIADA PARA RENDERIZAR
+    } else {
+        const emailUser = await User.findOne({email:email});
+        if(emailUser) {
+            const emailExistMessage = 'Email already registered!'
+            console.log(emailExistMessage);
+            res.redirect('/users/signup');
+        }
+        const newUser = new User({name, email, password});
+        newUser.password = await newUser.encryptPassword(password);
+        await newUser.save();
+        const userCreationMessage = 'User successfully created!';
+        console.log(userCreationMessage);
 
+        res.redirect('/users/signin');
+    }
+});
 
-router.route ('/login')
-.post (login)
-
-
-router.route ('/:id')
-.get (passport.authenticate('jwt', { session: false }),getUser)
-.delete (passport.authenticate('jwt', { session: false }), deleteUser )
-.put (passport.authenticate('jwt', { session: false }), updateUser)
-
-router.route ('/userbyusername/:id')
-.get (passport.authenticate('jwt', { session: false }), getUserByUsername)
-
-
-router.route ('/private', passport.authenticate('jwt', { session: false }), (req, res) => {
-    res.status (200).send ({message: 'access allowed'})
-})
-
-
-router.route ('/logout')
-
-.post(function(req, res){
+router.get('/users/logout', (req,res) => {
     req.logout();
     res.redirect('/');
-  });
-
-
+});
 
 module.exports = router;
