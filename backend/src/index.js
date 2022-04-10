aa6104932f4f9bdc909b5adf6163c80c2d86e03f
Erick-Rangel <rebel863@hotmail.com>
2022-04-10 14:27:38 -0500

    cambios en el index del back

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,43 +8,4 @@ function main() {
   console.log("Server on port: " + app.get("port"));
 }
 
-app.use (express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(methodOverride('_method'));
-app.use(session({
-    secret:SESSIONS_SECRET, 
-    resave:true,
-    saveUninitialized:true
-}));
-app.use(passport.initialize());
-app.use(passport.session());
-
-
-//Global Variables
-
-// app.use((req, res, next) => {
-//     res.locals.user = req.user || null;
-//     next(); 
-// })
-
-
-
-/* function main () {
-app.use(require('./routes/index'));
-app.use(require('./routes/User'));
-
-
-    app.listen (app.get ('port')); 
-    console.log ('Server on port: ' + app.get ('port'));
-    
-} */
-
 main();
-
-
-
-
-
-
-
-
