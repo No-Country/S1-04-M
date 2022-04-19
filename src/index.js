18ea20cd6f57c3e10b3b75924be76cb5b6173a8c
Ezequiel-Ormachea <59696589+Ezequiel-Ormachea@users.noreply.github.com>
2022-04-18 23:54:43 -0300

    heroku

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config();
+
+const app = require("./app");
+const database = require("./database");
+
+function main() {
+  app.listen(app.get("port"));
+  console.log("Server on port: " + app.get("port"));
+}
+
+main();
---
+++ src/routes/cards.js
@@ -0,0 +1,73 @@
+const { Router } = require("express");
+const router = Router();
+const {
+  getCards,
+  createNew,
+  lastCardNumber,
+  firstCardNumber,
+  deleteCards,
+  getCardsbyUser,
+  getDestinationCardsbyUser,
+  createNewDestinationCard,
+  getDestinationCards,
+  deleteDestinationCards, 
+  deleteDestinationCardsbyId,
+  getLastCardNumber, 
+  addMoneytoCard,
+  getCardsById,
+  
+} = require("../controllers/cards.controller");
+const auth = require("../middlewares/auth");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+
+router
+  .route("/")
+
+  .get(getCards)
+  .post(createNew)
+  .delete(deleteCards);
+
+router
+  .route("/byUser/:id")
+  .get (getCardsbyUser)
+
+
+router
+  .route("/lastCardNumber")
+  .get (getLastCardNumber)
+  .post(lastCardNumber);
+
+router
+  .route("/firstCardNumber")
+  .post(firstCardNumber);
+  
+router
+  .route("/destinationCards")
+  .get (getDestinationCards) 
+  .post(createNewDestinationCard)
+  .delete(deleteDestinationCards)
+
+router
+  .route("/destinationCards/byUser/:id")
+  .get(getDestinationCardsbyUser);
+
+router
+  .route("/destinationCards/byId")
+  .delete(deleteDestinationCardsbyId);
+
+router
+  .route("/addMoney")
+  .post(addMoneytoCard);
+
+router  
+   .route ("/byId/:id")
+   .get (getCardsById) 
+
+  
+
+ //.get ( passport.authenticate('jwt', { session: false }), getCards)
+
+//.post (passport.authenticate('jwt', { session: false }), createCard )
+
+module.exports = router;
