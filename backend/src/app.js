9b107adb39e44901cef558b31bccddb1a22c5e80
EnzoGRDev <enzo.g.rz.18@gmail.com>
2022-04-21 18:24:07 -0300

    configurando cors para el servidor de front

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,11 @@ const bodyParser = require("body-parser");
 app.set("port", process.env.PORT || 4000);
 
 //middlewares
-app.use(cors()); //cors hace q dos servidores se conecten entre ellos (back y front por ej)
+app.use(
+  cors({
+    origin: ["https://bankforyou.vercel.app"],
+  })
+); //cors hace q dos servidores se conecten entre ellos (back y front por ej)
 app.use(express.json()); //para q las rutas entiendan json y strings
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
